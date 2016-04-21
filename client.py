43ce48f4946bebdc7c4dc7119da1f7a68f99f945
Zhi Qian Seah <Zhi Qian Seah>
2016-04-21 00:05:45 -0400

    set up data structures, and did queryBalance and queryPrice

--- client.py
+++ client.py
@@ -1,13 +1,27 @@
 import socket
+import json			#for serializing data
 
 class PlayerClient:
 
 	#initialize the Player client with a host/port
-	def __init__(self, host = "127.0.0.1", port = 9999):
+	def __init__(self, username = 'user1', host = "127.0.0.1", port = 40000):
 
 		#Set up Server socket
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		sock.connect((host, port))
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.connect((host, port))
+		self.user = username
+
+		#run a loop to get user commands
+		self.CommandLoop()
+
+	def CommandLoop(self):
+
+		#the first message to the server is the username to identify the user
+		self.sock.send(self.user)
+
+		#get reply from the server regarding the login
+		msg = self.sock.recv(1024)
+		print msg
 
 		#while loop to get user input and set to the server
 		while True:
@@ -20,15 +34,56 @@ class PlayerClient:
 				break
 
 			try:
+
+				command_dict = self.Parse_Input(msg)
+
+				#Serialize the data
+				data_string = json.dumps(command_dict)
+				#print data_string
+
 				#send the msg to the socket
-				sock.send(msg)
+				self.sock.send(data_string)
 
 				#get reply from the server and print it
-				msg = sock.recv(1024)
-				print "Received:", msg 
-			except:
+				reply_raw = self.sock.recv(1024)
+				msg = json.loads(reply_raw) 
+				self.Parse_Print_Reply(msg)
+
+			except Exception as e:
+				print "Exception:", e
 				print "Connection Broken. Quitting program"
+				self.sock.close()
 				break
 
+	#this function parse the command line input string from the user and populates a command dictionary accordingly
+	def Parse_Input(self, msg):
+		command_dict = {}
+
+		#split the message based on spaces
+		msg_split = msg.split(" ")
+
+		#store it in the relevant sections of the command dictionary
+		command_dict["request_type"] = msg_split[0]
+
+
+		return command_dict
+
+	#This function parse the reply dictionary from the server, and print the relevant information
+	def Parse_Print_Reply(self, msg):
+
+		#parse the different type of response from the server
+		if msg['response_type'] == "invalidCommand":
+			print "Server received an invalid command."
+
+		if msg['response_type'] == "queryBalanceResponse":
+			print "Your Bank Balance is:", msg['data']['balance']
+
+		if msg['response_type'] == 'queryPriceResponse':
+			print "Prices of Companies:"
+			for company in msg['data']:
+				print company + ":", msg['data'][company] 
+		#print "Received:", msg
+
 if __name__ == "__main__":
-    PlayerClient()
+    client = PlayerClient(username = 'user1')
+    
--- server.py
+++ server.py
@@ -1,41 +1,106 @@
 import socket
-import thread
+import thread 		#for spawning new threads to handle clients
+import json			#for serializing data
+
 
 class StockExchangeServer:
 
 	#initialize the Stock Exchange Server with a host/port
-	def __init__(self, host = "127.0.0.1", port = 9999):
+	def __init__(self, host = "127.0.0.1", port = 40000, Num_Companies = 5, StartingPrice = 30):
 
 		#Set up Server socket
-		sock = socket.socket()
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock.bind((host, port))
-
 		print "Server Socket initialized."
+
+		self.account = {}
+		self.companies = {}
+		#populate the companies with initial values
+		for x in range(Num_Companies):
+			self.companies['Company'+str(x)] = StartingPrice
+
 
 		#Listen for connections from clients. 
 		#For each connection, spawn a new thread to handle  that client
-		while True:
-			sock.listen(1)
-			client, addr = sock.accept()     # Establish connection with client.
-			print 'Got connection from', addr	
-			thread.start_new_thread( self.client_handling_thread, (client, addr ) )
+		try:
+			while True:
+				sock.listen(1)
+				client, addr = sock.accept()     # Establish connection with client.
+				print 'Got connection from', addr	
+				thread.start_new_thread( self.Client_Handling_Thread, (client, addr ) )
 
-
+		except KeyboardInterrupt:
+			print "Server Ended."
+    		sock.close()
 
 
  	#thread to handle an incoming client
- 	def client_handling_thread(self, client, addr):
+ 	def Client_Handling_Thread(self, client, addr):
+
+ 		#the first message received by the user is his username to identify himself
+		username = client.recv(1024)		
+		print addr, ' >> ', username
+
+		#if the stock exchange sees a new user, create a new account for him and give him 1000 dollars
+		#Reply the client with a welcome message
+		if username not in self.account:
+			self.account[username] = {}
+			self.account[username]['bank'] = 1000
+	 		client.send("Welcome, new user!. We have created a new account for you.")
+	 	else:
+		 	client.send("Welcome Back, "+str(username)+".") 		
+
 
  		#listen to the client messages and respond accordingly
  		while True:
  			try:
-				msg = client.recv(1024)
-				print addr, ' >> ', msg
-	 			client.send("Server Received: "+msg)
-	 		except:
+
+ 				#receive the data from the client
+				msg_raw = client.recv(1024)
+
+				#deserialize the data into a dictionary
+				msg_dict = json.loads(msg_raw) 
+				print addr, ' >> ', msg_dict
+
+				#process the message
+				return_msg = self.Process_Message(msg_dict, username)
+
+
+
+
+				data_string = json.dumps(return_msg)
+				#send an acknowledgement back to the server
+	 			client.send(data_string)
+
+	 		except Exception as e:
+	 			print "Exception: ", e
 	 			print "Connection Broken from:", addr
 	 			break
 
+
+	#This function process an incoming command dictionary from the client, and process it accordingly
+	#It will reply with a response dictionary that will be sent back to the user
+	def Process_Message(self, msg_dict, username):
+
+		reply_dict = {}
+
+		if msg_dict['request_type'] == "queryBalance":
+			reply_dict['response_type'] = "queryBalanceResponse"
+			reply_dict['data'] = {}
+			reply_dict['data']['balance'] = self.account[username]['bank']			
+			return reply_dict
+
+		elif msg_dict['request_type'] == "queryPrice":
+			reply_dict['response_type'] = "queryPriceResponse"
+			reply_dict['data'] = {}		
+			for company in self.companies:
+				reply_dict['data'][company] = self.companies[company]
+			return reply_dict
+
+		#opcode not recognized. return invalid command
+		reply_dict['response_type']= "invalidCommand"
+		reply_dict['data'] = {}
+		return reply_dict
 
 if __name__ == "__main__":
     StockExchangeServer()
